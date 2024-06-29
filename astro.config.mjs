1f05ce00335d9e4bbda6ce5e2552b168ce811373
Lisandra Cruz <lisandra.cruz@thoughtworks.com>
2024-06-28 22:29:22 -0300

    chore: setup project

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import astroI18next from "astro-i18next";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [astroI18next({
+    baseLanguage: "pt",
+    i18next: {
+      debug: true, 
+      supportedLngs: ["pt", "en"],
+    },
+  })],
+});
