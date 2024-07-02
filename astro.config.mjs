44b3e03bccbd547b7568eef80d91fe387280caee
Lisandra Cruz <lisandra.cruz@thoughtworks.com>
2024-07-02 20:38:05 -0300

    add tailwind and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
+import tailwind from '@astrojs/tailwind';
 import astroI18next from "astro-i18next";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -6,8 +8,8 @@ export default defineConfig({
   integrations: [astroI18next({
     baseLanguage: "pt",
     i18next: {
-      debug: true, 
+      debug: true,
       supportedLngs: ["pt", "en"],
     },
-  })],
+  }), tailwind(), icon()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		colors: {
+			primary: '#0072C6',
+			white: '#FFFFFF',
+			black: '#000000',
+			gray: "#00000080",
+		},
+		fontFamily: {
+			sans: ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
+		},
+	}
 }
